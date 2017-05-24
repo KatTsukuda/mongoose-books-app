b09f51820a29592c87ddb3f1fdcda138b49ca9f0
Kat Tsukuda <BearHeartsBat@Kats-MacBook-Pro.local>
2017-05-24 16:22:19 -0700

    verified that seed.js works

--- models/index.js
+++ models/index.js
@@ -3,4 +3,4 @@ var mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost/book-app");
 
 // export Book
-module.exports.Book = require('.book.js')
+module.exports.Book = require('./book.js')
