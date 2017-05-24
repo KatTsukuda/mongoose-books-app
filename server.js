5a6c0201548f5dd5336e5ff9fdae84489b0b46f5
Kat Tsukuda <BearHeartsBat@Kats-MacBook-Pro.local>
2017-05-24 16:32:55 -0700

    removed hard coded books array from server.js

--- server.js
+++ server.js
@@ -9,8 +9,8 @@
 //require express in our app
 var express = require('express'),
   bodyParser = require('body-parser');
- 
-var db = require('./models')
+
+var db = require('./models');
 
 // generate a new express app and call it 'app'
 var app = express();
@@ -27,32 +27,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //  DATA
 ///////////////////
 
-var books = [
-  {
-    _id: 15,
-    title: "The Four Hour Workweek",
-    author: "Tim Ferriss",
-    image: "https://s3-us-west-2.amazonaws.com/sandboxapi/four_hour_work_week.jpg",
-    release_date: "April 1, 2007"
-  },
-  {
-    _id: 16,
-    title: "Of Mice and Men",
-    author: "John Steinbeck",
-    image: "https://s3-us-west-2.amazonaws.com/sandboxapi/of_mice_and_men.jpg",
-    release_date: "Unknown 1937"
-  },
-  {
-    _id: 17,
-    title: "Romeo and Juliet",
-    author: "William Shakespeare",
-    image: "https://s3-us-west-2.amazonaws.com/sandboxapi/romeo_and_juliet.jpg",
-    release_date: "Unknown 1597"
-  }
-];
 
-
-var newBookUUID = 18;
 
 
 
@@ -75,8 +50,13 @@ app.get('/', function (req, res) {
 // get all books
 app.get('/api/books', function (req, res) {
   // send all books as JSON response
-  console.log('books index');
-  res.json(books);
+  db.Book.find(function(err,books) {
+      if (err) {
+          console.log('index error: ' + err);
+          res.sendStatus(500);
+      }
+      res.json(books);
+  });
 });
 
 // get one book
