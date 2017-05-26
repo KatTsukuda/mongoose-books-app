33482b0411ff17e2975c5ef1890f37b74cc809ee
Kat Tsukuda <BearHeartsBat@Kats-MacBook-Pro.local>
2017-05-25 21:02:04 -0700

    change routes on server.js

--- server.js
+++ server.js
@@ -71,16 +71,41 @@ app.get('/api/books/:id', function (req, res) {
   }
 });
 
+// get all books
+app.get('/api/books', function (req, res) {
+  // send all books as JSON response
+  db.Book.find()
+    // populate fills in the author id with all the author data
+    .populate('author')
+    .exec(function(err, books){
+      if (err) { return console.log("index error: " + err); }
+      res.json(books);
+    });
+});
+
 // create new book
 app.post('/api/books', function (req, res) {
   // create new book with form data (`req.body`)
-  console.log('books create', req.body);
-  var newBook = req.body;
-  newBook._id = newBookUUID++;
-  books.push(newBook);
-  res.json(newBook);
-});
+  var newBook = new db.Book({
+    title: req.body.title,
+    image: req.body.image,
+    releaseDate: req.body.releaseDate,
+  });
 
+  // this code will only add an author to a book if the author already exists
+  db.Author.findOne({name: req.body.author}, function(err, author){
+    newBook.author = author;
+    // add newBook to database
+    newBook.save(function(err, book){
+      if (err) {
+        return console.log("create error: " + err);
+      }
+      console.log("created ", book.title);
+      res.json(book);
+    });
+  });
+
+});
 // update book
 app.put('/api/books/:id', function(req,res){
 // get book id from url params (`req.params`)
